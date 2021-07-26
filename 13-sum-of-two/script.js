97d12b9ec89664a0cf78b10f8bded7c440faa17a
Konstantin Nikolavih <KonstantinNikolavih@yandex.ru>
2021-07-26 09:13:41 +0300

    исправления

--- 13-sum-of-two/script.js
+++ 13-sum-of-two/script.js
@@ -9,10 +9,21 @@
 */
 
 function sumOfTwo(arr, sum) {
-    // Напишите код здесь
-}
+  if ((Array.isArray(arr)) && (typeof sum === 'number')) {
+  let a = false;
+  firstIteration:
+  for (let i = 0; i < arr.length; i++) {
+  for(let j = 1; j < arr.length; j++) {
+  if (sum === arr[i] + arr[j]) { a = true;
+  break firstIteration;
+  }
+}};
+  return a;
+ } else {
+   return a;
+   }
+};
 
-// Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(sumOfTwo([1, 2, 3, 4, 5], 4)); // true (так как 1 + 3 === 4)
 console.log(sumOfTwo([1, 2, 3, 4, 5], 100)); // false
