6b685e157ffcd975c1848fbb8c3d2e988fcf7f76
Konstantin Nikolavih <KonstantinNikolavih@yandex.ru>
2021-07-26 09:42:50 +0300

    14 задача

--- 14-primes/script.js
+++ 14-primes/script.js
@@ -7,10 +7,29 @@
 */
 
 function primes(num) {
-    // Напишите код здесь
-}
-
-// Протестируйте решение, вызывая функцию с разными аргументами:
+  if (num > 1) {
+  if (num === 2) {
+    return 2;
+  } else if (num === 3) {
+    return [2, 3];
+    } else {
+    let numbers = [2, 3];
+    firstLoop:
+    for (let i = 4; i <= num; i++) {   
+    const a = Math.floor(Math.sqrt(i));
+    for (let j = 2; j <= a; j++) {
+    if (i % j === 0) {       
+     continue firstLoop;                     
+    }
+    }
+  numbers.push(i);
+  }
+  return numbers;
+  }
+  } else {
+  return [];
+  }
+};
 
 console.log(primes(6)); // [2, 3, 5]
 console.log(primes(17)); // [2, 3, 5, 7, 11, 13, 17]
